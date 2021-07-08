d9f3e1a7dd738ef966b6d9cde2acb8a9bb788dfc
Anri17 <andralmeida1999@gmail.com>
2021-07-08 14:04:25 +0100

    Added Scroll down animation top scroll button and links

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,11 @@ for (let i = 0; i < audioElements.length; i++) {
     const element = audioElements[i];
     element.volume = 0.5;
 }
+
+// Scroll down button
+
+
+// testing
+function scrollDown(destinationId) {
+    $('html, body').animate({ scrollTop: $(destinationId).offset().top}, 500);
+}
