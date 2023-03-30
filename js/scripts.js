2ddaf36cd7bd0f8e5e33f3fbbb486f0cea845165
Anri17 <andralmeida1999@gmail.com>
2023-03-30 19:02:22 +0100

    Add burger menu for smaller screen width sizes

--- js/scripts.js
+++ js/scripts.js
@@ -12,6 +12,17 @@ function scroll_down(destination_id) {
 
 let navbar_title_size_min = "2rem";
 let navbar_title_size_max = "3.5rem";
+const nav_burger_button = document.getElementById("nav_burger_button");
+let burger_is_active = false;
+
+window.onload = () => {
+
+    if (burger_is_active) {
+        nav_burger_container.style.height = "100%";
+    } else {
+        nav_burger_container.style.height = "0%";
+    }
+}
 
 window.onresize = () => {
     navbar_title_resize();
@@ -19,6 +30,18 @@ window.onresize = () => {
 
 window.onscroll = () => {
     navbar_title_resize();
+}
+
+nav_burger_button.onclick = () => {
+    let nav_burger_container = document.getElementById("nav_burger_container");
+
+    burger_is_active = !burger_is_active;
+
+    if (burger_is_active) {
+        nav_burger_container.style.height = "100%";
+    } else {
+        nav_burger_container.style.height = "0%";
+    }
 }
 
 function navbar_title_resize()
